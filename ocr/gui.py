19388a7c0a71b98a0f34eb7e7a91a5960c3944ab
U-Anthony-PC\Anthony <donaldsa18@up.edu>
2020-01-05 03:31:43 -0800

    added functionality to hide fissures table and hide relics

--- ocr/gui.py
+++ ocr/gui.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QTableWidget, QWidget, QVBoxLayout, QLabel, QAbstractItemView, QHBoxLayout, \
     QSlider, QGridLayout, QGroupBox, QCheckBox, QHeaderView, QPushButton, QProgressBar, QTableWidgetItem, QDialog
 from PyQt5.QtGui import QIcon, QPixmap, QImage
-from PyQt5.QtCore import Qt, QThread
+from PyQt5.QtCore import Qt, QThread, QTimer
 import qdarkstyle
 from functools import partial
 from ocr import OCR
@@ -51,10 +51,10 @@ class Window(QWidget):
         mission_header.resizeSection(3, 60)
         self.mission_table.setFixedWidth(405)
 
-        self.slider_names = ['x', 'y', 'w', 'h', 'v1', 'v2']
+        self.slider_names = ['x', 'y', 'w', 'h', 'v1', 'v2', 'Screencap', 'Fissure']
         self.sliders = {x: QSlider(Qt.Horizontal) for x in self.slider_names}
         slider_labels = {x: QLabel(x) for x in self.slider_names}
-        self.slider_default_values = {'x': 521, 'y': 400, 'w': 908, 'h': 70, 'v1': 197, 'v2': 180}
+        self.slider_default_values = {'x': 521, 'y': 400, 'w': 908, 'h': 70, 'v1': 197, 'v2': 180, 'Screencap': 1, 'Fissure': 30}
         self.slider_values = {x: QLabel(str(self.slider_default_values[x])) for x in self.slider_names}
 
         self.sliders['x'].setMaximum(int(warframe_width / 2))
@@ -70,6 +70,11 @@ class Window(QWidget):
             self.slider_values[slider_name].setFixedWidth(35)
             self.sliders[slider_name].setValue(self.slider_default_values[slider_name])
 
+        self.sliders['Screencap'].setMaximum(5)
+        self.sliders['Screencap'].setMinimum(1)
+        self.sliders['Fissure'].setMaximum(60)
+        self.sliders['Fissure'].setMinimum(10)
+
 
         self.is_slider_max_set = False
 
@@ -80,28 +85,20 @@ class Window(QWidget):
         self.pause_button = QPushButton("Pause")
         self.pause_button.clicked.connect(self.toggle_button)
         self.is_paused = False
-        self.pref_grid.addWidget(self.pause_button, 0, 0, 1, 3)
+        #self.pref_grid.addWidget(self.pause_button, 0, 0, 1, 3)
 
         self.plat_check_box = QCheckBox("Prefer platinum")
         self.plat_check_box.setChecked(True)
-        self.pref_grid.addWidget(self.plat_check_box, 1, 0, 1, 3)
-
-        self.hide_crop_check_box = QCheckBox("Hide Crop")
-        self.hide_crop_check_box.setChecked(False)
-        self.hide_crop_check_box.stateChanged.connect(self.toggle_cropped_img)
-        self.pref_grid.addWidget(self.hide_crop_check_box, 2, 0, 1, 3)
-
-        self.hide_filter_check_box = QCheckBox("Hide Filtered")
-        self.hide_filter_check_box.setChecked(False)
-        self.hide_filter_check_box.stateChanged.connect(self.toggle_filtered_img)
-        self.pref_grid.addWidget(self.hide_filter_check_box, 2, 2, 1, 3)
+        #self.pref_grid.addWidget(self.plat_check_box, 1, 0, 1, 3)
 
         update_layout = QGridLayout()
         update_layout.setColumnStretch(4, 2)
-        update_layout.setContentsMargins(7, 7, 7, 60)
+        update_layout.setAlignment(Qt.AlignTop)
+        update_layout.setContentsMargins(0, 0, 0, 0)
 
         update_prices_button = QPushButton("Update Prices")
         update_prices_progress = QProgressBar()
+        update_prices_progress.setFixedWidth(110)
         update_layout.addWidget(update_prices_button, 0, 0)
         update_layout.addWidget(update_prices_progress, 0, 1)
 
@@ -122,19 +119,101 @@ class Window(QWidget):
 
         update_box = QGroupBox("Updates")
         update_box.setLayout(update_layout)
-        update_box.setFixedWidth(190)
-        update_box.setFixedHeight(243)
-        i = 4
-        for slider_name in self.slider_names:
-            self.pref_grid.addWidget(slider_labels[slider_name], i, 0)
-            self.pref_grid.addWidget(self.slider_values[slider_name], i, 1)
-            self.pref_grid.addWidget(self.sliders[slider_name], i, 2)
-            i = i + 1
 
-        self.pref_box = QGroupBox("Preferences")
-        self.pref_box.setLayout(self.pref_grid)
-        self.pref_box.setFixedWidth(190)
-        self.pref_box.setFixedHeight(243)
+        crop_grid = QGridLayout()
+        crop_grid.setColumnStretch(3, 4)
+        crop_grid.setAlignment(Qt.AlignTop)
+        crop_grid.setContentsMargins(0, 0, 0, 0)
+        for i in range(4):
+            slider_name = self.slider_names[i]
+            crop_grid.addWidget(slider_labels[slider_name], i, 0)
+            crop_grid.addWidget(self.slider_values[slider_name], i, 1)
+            crop_grid.addWidget(self.sliders[slider_name], i, 2)
+
+        crop_box = QGroupBox("Crop Parameters")
+        crop_box.setLayout(crop_grid)
+
+        filter_grid = QGridLayout()
+        filter_grid.setColumnStretch(3, 2)
+        filter_grid.setAlignment(Qt.AlignTop)
+        filter_grid.setContentsMargins(0, 0, 0, 0)
+        for i in range(2):
+            slider_name = self.slider_names[i+4]
+            filter_grid.addWidget(slider_labels[slider_name], i, 0)
+            filter_grid.addWidget(self.slider_values[slider_name], i, 1)
+            filter_grid.addWidget(self.sliders[slider_name], i, 2)
+
+        filter_box = QGroupBox("Filter Parameters")
+        filter_box.setLayout(filter_grid)
+
+        settings_layout_1 = QVBoxLayout()
+        settings_layout_1.addWidget(crop_box)
+        settings_layout_1.addWidget(filter_box)
+
+        rate_grid = QGridLayout()
+        rate_grid.setColumnStretch(3, 2)
+        rate_grid.setContentsMargins(0, 0, 0, 0)
+        for i in range(2):
+            slider_name = self.slider_names[i+6]
+            rate_grid.addWidget(slider_labels[slider_name], i, 0)
+            rate_grid.addWidget(self.slider_values[slider_name], i, 1)
+            rate_grid.addWidget(self.sliders[slider_name], i, 2)
+
+        rate_box = QGroupBox("Rates")
+        rate_box.setLayout(rate_grid)
+
+        settings_layout_2 = QVBoxLayout()
+        settings_layout_2.addWidget(update_box)
+        settings_layout_2.addWidget(rate_box)
+        settings_layout_2.addWidget(self.pause_button)
+
+        hide_layout = QVBoxLayout()
+        hide_layout.setAlignment(Qt.AlignTop)
+        hide_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.hide_crop_check_box = QCheckBox("Hide Crop")
+        self.hide_crop_check_box.setChecked(False)
+        self.hide_crop_check_box.stateChanged.connect(self.toggle_cropped_img)
+        hide_layout.addWidget(self.hide_crop_check_box)
+
+        self.hide_filter_check_box = QCheckBox("Hide Filtered")
+        self.hide_filter_check_box.setChecked(False)
+        self.hide_filter_check_box.stateChanged.connect(self.toggle_filtered_img)
+        hide_layout.addWidget(self.hide_filter_check_box)
+
+        self.hide_fissure_check_box = QCheckBox("Hide Fissure Table")
+        self.hide_fissure_check_box.setChecked(False)
+        self.hide_fissure_check_box.stateChanged.connect(self.toggle_fissure_table)
+        hide_layout.addWidget(self.hide_fissure_check_box)
+
+        hide_box = QGroupBox("Hide UI")
+        hide_box.setLayout(hide_layout)
+
+        hide_relics_layout = QVBoxLayout()
+        hide_relics_layout.setAlignment(Qt.AlignTop)
+        hide_relics_layout.setContentsMargins(0, 0, 0, 0)
+        relics = ["Axi", "Neo", "Meso", "Lith", "Requiem"]
+        self.hide_relics = {}
+        for relic in relics:
+            self.hide_relics[relic] = QCheckBox(relic)
+            self.hide_relics[relic].setChecked(False)
+            self.hide_relics[relic].stateChanged.connect(partial(self.set_hidden_relic, relic))
+            hide_relics_layout.addWidget(self.hide_relics[relic])
+
+        hide_relics_box = QGroupBox("Hide Relics")
+        hide_relics_box.setLayout(hide_relics_layout)
+
+        self.hidden_relics = set()
+
+        settings_layout_3 = QVBoxLayout()
+        settings_layout_3.addWidget(hide_box)
+        settings_layout_3.addWidget(hide_relics_box)
+
+
+        self.settings_layout = QHBoxLayout()
+        self.settings_layout.addLayout(settings_layout_1)
+        self.settings_layout.addLayout(settings_layout_2)
+        self.settings_layout.addLayout(settings_layout_3)
 
         bot_layout.addWidget(self.table)
         bot_layout.addWidget(self.mission_table)
@@ -190,13 +269,24 @@ class Window(QWidget):
         self.dialog = QDialog()
         self.dialog.setWindowTitle("Preferences")
         self.dialog.setWindowModality(Qt.ApplicationModal)
-        self.dialog.setLayout(self.pref_grid)
+        self.dialog.setLayout(self.settings_layout)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_mission_table_time)
+        self.timer.start(1000)
 
         self.show()
         self.setFixedSize(self.layout.sizeHint())
 
     def show_preferences(self):
         self.dialog.exec_()
+
+    def toggle_fissure_table(self, checkbox):
+        if self.hide_fissure_check_box.isChecked():
+            self.mission_table.hide()
+        else:
+            self.mission_table.show()
+        self.setFixedSize(self.layout.sizeHint())
 
     def toggle_cropped_img(self, checkbox):
         if self.hide_crop_check_box.isChecked():
@@ -267,6 +357,10 @@ class Window(QWidget):
                 self.mission_table.setItem(i, j, QTableWidgetItem(str(self.missions[i][j])))
 
             self.mission_table.setItem(i, 3, QTableWidgetItem(self.get_duration_str(self.missions[i][3]-cur_time)))
+            if self.missions[i][0] in self.hidden_relics:
+                self.mission_table.setRowHidden(i, True)
+            else:
+                self.mission_table.setRowHidden(i, False)
 
         self.mission_table.setRowCount(len(self.missions)-1)
 
@@ -274,6 +368,13 @@ class Window(QWidget):
         cur_time = time.time()
         for i in range(len(self.missions)):
             self.mission_table.setItem(i, 3, QTableWidgetItem(self.get_duration_str(self.missions[i][3]-cur_time)))
+
+    def update_mission_table_hidden(self):
+        for i in range(len(self.missions)):
+            if self.missions[i][0] in self.hidden_relics:
+                self.mission_table.setRowHidden(i, True)
+            else:
+                self.mission_table.setRowHidden(i, False)
 
     def get_duration_str(self,duration):
         m, s = divmod(int(duration), 60)
@@ -284,6 +385,13 @@ class Window(QWidget):
         for slider_name in self.slider_names:
             self.sliders[slider_name].valueChanged.connect(partial(self.set_ocr_crop, ocr, slider_name))
         self.ocr = ocr
+
+    def set_hidden_relic(self, relic, checkbox):
+        if self.hide_relics[relic].isChecked():
+            self.hidden_relics.add(relic)
+        else:
+            self.hidden_relics.remove(relic)
+        self.update_mission_table_hidden()
 
     def set_ocr_crop(self, ocr, dim, val):
         self.slider_values[dim].setNum(val)
@@ -369,18 +477,18 @@ class APIThread(QThread):
 class TableThread(QThread):
     def __init__(self, gui):
         QThread.__init__(self)
-        self.scheduler = sched.scheduler(time.time, time.sleep)
+        #self.scheduler = sched.scheduler(time.time, time.sleep)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update)
         self.gui = gui
 
     def __del__(self):
         self.wait()
 
     def run(self):
-        self.scheduler.enter(1, 1, self.update)
-        self.scheduler.run()
+        self.timer.start(1000)
 
     def update(self):
-        self.scheduler.enter(1, 1, self.update)
         self.gui.update_mission_table_time()
 
 app = QApplication([])
@@ -396,7 +504,7 @@ thread.start()
 api_thread = APIThread(window)
 api_thread.start()
 
-table_thread = TableThread(window)
-table_thread.start()
+#table_thread = TableThread(window)
+#table_thread.start()
 
 app.exec_()
